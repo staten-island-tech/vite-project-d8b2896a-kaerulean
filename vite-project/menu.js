7d772431fc5a02831a82a24c69ced00f90d6e1c6
katherine <116115224+kaerulean@users.noreply.github.com>
2023-11-16 13:15:24 -0500

    Summary (required)
    peeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

---
+++ vite-project/menu.js
@@ -0,0 +1,90 @@
+//ADD PRICE AND IMAGE AS A PROPERTY
+
+const menuItems = [
+    {
+        name: "french fries",
+        type: "appetizer",
+        origin: "europe",
+        vegan: true,
+    },
+
+    {
+        name: "dumplings",
+        type: "appetizer",
+        origin: "asia",
+        vegan: false,
+    },
+
+    {
+        name: "chicken wings",
+        type: "appetizer",
+        origin: "americas",
+        vegan: false,
+    },
+
+    {
+        name: "mac and cheese",
+        type: "side",
+        origin: "europe",
+        vegan: false,
+    },
+
+    {
+        name: "steamed egg pudding",
+        type: "side",
+        origin: "asia",
+        vegan: false,
+    },
+
+    {
+        name: "pizza",
+        type: "main",
+        origin: "europe", 
+        vegan: false,
+    },
+
+    {
+        name: "roast turkey",
+        type: "main",
+        origin: "americas", 
+        vegan: false,
+    },
+
+    {
+        name: "kbbq",
+        type: "main",
+        origin: "asia", 
+        vegan: false,
+    },
+
+    {
+        name: "enchiladas",
+        type: "main",
+        origin: "americas", 
+        vegan: true,
+    },
+
+    {
+        name: "fruit",
+        type: "dessert",
+        origin: "asia", 
+        vegan: true,
+    },
+
+    {
+        name: "gelato",
+        type: "dessert",
+        origin: "europe", 
+        vegan: false,
+    },
+
+    {
+        name: "mango sticky rice",
+        type: "dessert",
+        origin: "asia", 
+        vegan: true,
+    },
+
+]
+
+export {menuItems}
